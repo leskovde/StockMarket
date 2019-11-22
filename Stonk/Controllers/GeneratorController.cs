fe19f1a910bebb22b1fe3061b8b3042a1e9ae294
leskovde <47887202+leskovde@users.noreply.github.com>
2019-11-22 11:32:13 +0100

    Add files via upload

---
+++ Stonk/Controllers/GeneratorController.cs
@@ -0,0 +1,217 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stonk.Models;
+using Stonk.Models.Persons;
+using Stonk.Plugins.Database;
+using Stonks.Plugins.Generator;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Stonks.Controllers
+{
+    //[Route("[controller]/[action]")]
+    public class GeneratorController : Controller
+    {
+        private readonly Database _context;
+
+
+
+
+        private ulong Ticks { get; set; } = 0;
+
+        List<Stock> LastIteration { get; set; } = new List<Stock>() { new Stock() { id = 10, firstValue = 10, description = "asas" } };  
+        decimal TaxRate { get; set; }
+        List<StockDependency> Dependencies { get; set; }
+
+        private int IterationsPerTicks { get; set; } = 1;
+        //List<Stock>  { get; set; }
+
+        ConcurrentDictionary<int, int> transactions = new ConcurrentDictionary<int, int>();
+        private readonly object _lock = new object();
+
+        public IActionResult Index()
+        {
+            return new OkResult();
+        }
+        public GeneratorController(Database context)
+        {
+            _context = context;
+            
+            Dependencies = new List<StockDependency>();  //TODO add dependencies
+            timer.Elapsed += (a, e) => OneTick();
+            //timer.Start();
+            timer.Enabled = false;
+        }
+
+        Timer timer { get; } = new Timer(1);
+
+        async Task OneTick()
+        {
+            Generate();
+            foreach (var item in LastIteration)
+            {
+               // _context.Update(item);
+            }
+            //await _context.SaveChangesAsync();
+            Ticks++;
+        }
+
+
+        // Get: Generator/CurrentGameState
+        [HttpGet]
+        public async Task<IActionResult> CurrentGameState()
+        {
+            var tmp = LastIteration; //cache the collection
+            return View(tmp);
+
+        }
+
+        // Get: Generator/CurrentTime
+        [HttpGet]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> CurrentTime()
+        {
+            var tmp = LastIteration; //cache the collection
+            return Json(Ticks);
+
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetStockWithTime()
+        {
+            Generate();
+            dynamic obj = LastIteration.First().firstValue;
+            return Json(obj);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStockWithTime(int id)
+        {
+             Generate();                 //cache the collection
+            return Json(LastIteration.First().firstValue);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> CurrentSingleStock(int id)
+        {
+            var tmp = Generator.RandomlyModify( LastIteration.First());                 //cache the collection
+            return View(tmp);
+        }
+
+        // POST: Generator/BuyShares
+        [HttpPost("{userID}/{stockID}/{amount}/")]
+        public async Task<IActionResult> BuyShares(int userID, int stockID, int amount)
+        {
+            var user = _context.Users.Find(userID);
+            var stock = _context.Stock.Find(stockID);
+
+            if (amount > 0 && user.userPortfolio.cash >= stock.firstValue * amount)
+            {
+                var portfolio = user.userPortfolio;
+                user.userPortfolio.cash -= stock.firstValue * amount;
+
+                if (user.userPortfolio.listOfShares.Any(x => x.id == stockID))
+                    user.userPortfolio.listOfShares.Find(x => x.id == stockID).amount += amount;
+                else
+                    user.userPortfolio.listOfShares.Add(new Share()
+                    {
+                        stockId = stockID,
+                        amount = amount,
+                        portfolioId = user.userPortfolio.id
+                    });
+
+                await _context.SaveChangesAsync();
+                transactions.AddOrUpdate(stockID, amount, (_, oldVal) => oldVal + amount);
+
+                return Ok();
+            }
+            return BadRequest();
+
+        }
+        // POST: Generator/BuyShares
+        [HttpPost("{userID}/{stockID}/{amount}/")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SellShares(int userID, int stockID, int amount)
+        {
+            var user = _context.Users.Find(userID);
+            var stock = _context.Stock.Find(stockID);
+
+            if (amount > 0 && user.userPortfolio.listOfShares.Find(x => x.id == stockID).amount >= amount)
+            {
+                var portfolio = user.userPortfolio;
+                user.userPortfolio.cash += stock.firstValue * amount * (1 - TaxRate);
+
+                if (user.userPortfolio.listOfShares.Any(x => x.id == stockID))
+                    user.userPortfolio.listOfShares.Find(x => x.id == stockID).amount += amount;
+                else
+                    user.userPortfolio.listOfShares.Add(new Share()
+                    {
+                        stockId = stockID,
+                        amount = amount,
+                        portfolioId = user.userPortfolio.id
+                    });
+
+                await _context.SaveChangesAsync();
+                transactions.AddOrUpdate(stockID, -amount, (_, oldVal) => oldVal - amount);
+
+                return Ok();
+            }
+            return BadRequest();
+
+        }
+
+        void Generate()
+        {
+            var tmp = LastIteration.First();
+
+            LastIteration = new List<Stock>(){Generator.RandomlyModify(tmp)};
+        }
+
+
+        StockValueInTime StockToTimeStock(Stock item)
+        {
+            return new StockValueInTime()
+            {
+                stockId = item.id,
+                timestamp = Ticks,
+                value = item.firstValue
+            };
+        }
+
+        // both 'last' and 'dependencies' are expected to be (ascending)ordered by id 
+        Dictionary<int, double> PropagateDependencies(List<Stock> last, List<StockDependency> dependencies)
+        {
+            int i = 0;
+            var curr = last[i];
+            var dict = new Dictionary<int, double>();
+            foreach (var dependency in dependencies)
+            {
+                while (curr.id != dependency.SourceID)
+                {
+                    if (i != last.Count - 1)
+                    {
+                        var tmp = curr;
+                        curr = last[++i];
+                        if (tmp.id > curr.id)
+                            throw new Exception("stock-list is not ordered!");
+                    }
+                    else break;
+                }
+                if (dict.TryGetValue(dependency.TargetID, out var value))
+                {
+                    dict[dependency.TargetID] = value + dependency.multiplier * curr.growTrend;
+                }
+                else
+                {
+                    dict[dependency.TargetID] = dependency.multiplier * curr.growTrend;
+                }
+            }
+            return dict;
+        }
+
+    }
+}
---
+++ Stonk/Controllers/UsersController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Stonk.Models.Persons;
+using Stonk.Plugins.Database;
+using Stonk.Views.Users;
+using Stonk.Models;
+
+namespace Stonk.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly Database _context;
+
+        public UsersController(Database context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Portfolio(int? id)
+        {
+            var view = new PortfolioView();
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            view.user = user;
+
+            var portfolio = await _context.Portfolios.FirstOrDefaultAsync(m => m.userId == user.id);
+            if (portfolio == null)
+            {
+                return NotFound();
+            }
+            //var sharess = _context.Share.Select(i => new IEnumerable<Share>{ i.portfolioId, portfolio.id });
+            List<Share> shares = new List<Share>();
+            foreach (var item in _context.Share)
+            {
+                if (item.portfolioId == portfolio.id)
+                    shares.Add(item);
+            }
+            view.shares = shares;
+
+            List<Stock> stocks = new List<Stock>();
+            List<StockValueInTime> values = new List<StockValueInTime>();
+            foreach (var item in _context.Stock)
+            {
+                if (shares.Any(x => x.stockId == item.id && item.id != 0))
+                {
+                    stocks.Add(item);
+                    values.Add(await _context.StockValuesInTime.FirstAsync());//spatne ale fakci
+                }
+
+            }
+            view.stocks = stocks;
+            view.values = values;
+
+            return View(view);
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Users/Register
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([Bind("id,isAdmin,username,password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.isAdmin = false;
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                Portfolio portfolio = new Portfolio(user.id, 10000);
+                _context.Add(portfolio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Portfolio), user.id);
+            }
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,isAdmin,username,password")] User user)
+        {
+            if (id != user.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.id == id);
+        }
+    }
+}
